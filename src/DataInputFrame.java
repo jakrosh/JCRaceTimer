18b50a873484274afff9eed5926e757c2186f8c8
Jaikrish <Jaikrish@192.168.0.14>
2018-07-11 20:45:26 -0700

    First Commit
    setting github up

---
+++ src/DataInputFrame.java
@@ -0,0 +1,58 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+
+public class DataInputFrame {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DataInputFrame window = new DataInputFrame();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public DataInputFrame() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Next");
+		btnNewButton.setBounds(165, 179, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(165, 136, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(149, 11, 105, 30);
+		frame.getContentPane().add(textPane);
+	}
+}
